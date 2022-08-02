19144a4c9410412b08d040f8901db3b9d3e3c873
jortan02 <jortan2002@gmail.com>
2022-08-01 20:05:25 -0600

    Started on image model and context

---
+++ RecipeBoxWebApp/Data/ImageDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RecipeBoxWebApp.Models;
+
+namespace RecipeBoxWebApp.Data
+{
+    public class ImageDbContext : DbContext
+    {
+        public ImageDbContext(DbContextOptions<ImageDbContext> options) : base(options)
+        {
+
+        }
+    
+        public DbSet<ImageModel> Images { get; set; }
+    }
+}
---
+++ RecipeBoxWebApp/Models/ImageModel.cs
@@ -0,0 +1,17 @@
+﻿// https://www.youtube.com/watch?v=QpJvqiHl1Fo
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RecipeBoxWebApp.Models
+{
+    public class ImageModel
+    {
+        [Key]
+        public int Id { get; set; }
+        [Column(TypeName = "nvarchar(50)")]
+        public string Title { get; set; }
+        [Column(TypeName = "nvarchar(100)")]
+        public string ImageName { get; set; }
+    }
+}
